db959002f50d2de45bb077e883945bd9c94a26fb
jl_ja <jl_ja@JEFERSON>
2023-09-20 18:52:56 -0300

    Add project files.

---
+++ RestWithASPNETUdmy/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNETUdmy.Model;
+
+namespace RestWithASPNETUdmy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
---
+++ RestWithASPNETUdmy/Services/Implementations/PersonServiceImplemetation.cs
@@ -0,0 +1,67 @@
+﻿using RestWithASPNETUdmy.Model;
+
+namespace RestWithASPNETUdmy.Services.Implementations
+{
+    public class PersonServiceImplemetation : IPersonService
+    {
+        private List<Person> persons = new List<Person>();
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> list = new List<Person>();
+            for (int i = 0;i <8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+          
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "juca",
+                LastName = "Abee",
+                Address = "asdadad",
+                Gender = "Male"
+
+            };
+        }
+
+        public Person Update(Person person)
+        {
+           return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "person Name" + i,
+                LastName = "person LastName" + i,
+                Address = "Some Address" + i,
+                Gender = "Male"
+
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
